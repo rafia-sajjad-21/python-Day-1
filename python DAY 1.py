d88d848a0c9a5febb727186aef73a5b6a2d8056e
Rafia Sajjad <F24-1019@student.uoh.edu.pk>
2025-07-03 21:04:48 +0500

    Create python DAY 1.py

---
+++ python DAY 1.py
@@ -0,0 +1,8 @@
+
+num_int = int(input("Enter an integer: "))
+num_float = float(input("Enter a float: "))
+text = input("Enter a string: ")
+
+print(f"Integer: {num_int}")
+print(f"Float: {num_float:.2f}")
+print(f"String: {text}")
